21ac465841b4d8ecd1e09019d9d5877c8618ef6f
juanlulch <juanlulch@gmail.com>
2021-03-08 19:14:07 +0100

    error valido de nuevo cambio en rama

--- prueba1/src/cambio1/Cambio1.java
+++ prueba1/src/cambio1/Cambio1.java
@@ -3,5 +3,6 @@ package cambio1;
 public class Cambio1 {
 	public static void main(String[] args) {
 		System.out.println("Pruebas Gitl. Clase Cambiol");
+		System.out.println("Pruebas Gitl. Clase Cambio2");
 		}
 }
