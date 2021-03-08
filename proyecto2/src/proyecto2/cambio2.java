ff568ded9926d483fb4f7abd3884fd45d34cc73d
juanlulch <juanlulch@gmail.com>
2021-03-08 19:42:01 +0100

    valido

---
+++ proyecto2/src/proyecto2/cambio2.java
@@ -0,0 +1,11 @@
+package proyecto2;
+
+public class cambio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Clase Cambio2 en proyecto1");
+		System.out.println("Clase Cambio2 en proyecto1");
+	}
+
+}
